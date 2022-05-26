140a05facf02d72856bd61625c161ac0b62d0bec
Zeissao <zeissao@qq.com>
2022-05-26 14:07:17 +0800

    Update Polar_Coordinates_of_Prime_Numbers.py

--- Polar_Coordinates_of_Prime_Numbers.py
+++ Polar_Coordinates_of_Prime_Numbers.py
@@ -1,13 +1,20 @@
-def fdprnbs(a,b):
+def prime_nums(a,b):
     coordinates=[]
+    if (a==0 and (b==0 or b==1)) or (a==1 and b==1):
+        return coordinates
+    if (a==0 or a==1) and b==2:
+        coordinates.append(2)
+        return coordinates
     for i in range(a,b+1,1):
+        judge=0
         if i%2!=0:
-            coordinates.append(i)
             for j in range(2,int(i**0.5),1):
                 k=i%j
                 if k==0:
-                    coordinates.pop()
+                    judge=1
                     break
+            if judge==0:
+                coordinates.append(i)
             percentage='{:.2%}'.format(i/b)
             if i!=b:
                 spin=int(dt.datetime.now().strftime('%S'))
@@ -21,11 +28,7 @@ def fdprnbs(a,b):
                     print("计算中//",percentage,end='\r')
             else:
                 print("计算完成 ",percentage)
-    if a==0:
-        coordinates.pop(0)
-        coordinates.pop(0)
-    if a==1:
-        coordinates.pop(0)
+    coordinates[0]=2
     return coordinates
 print("此程序可以展示一定区间内的素数在极坐标中的分布情况，其中每一个坐标的极径与极角值均等于对应的素数值")
 from timeit import default_timer as timer
@@ -36,12 +39,12 @@ while data!="end":
     data=data.split()
     a=int(data[0])
     b=int(data[1])
-    if b-a>=6000000:
+    if b-a>=100000000:
         print("如果你一定要绘制这个图像，请确保你的计算机在电量耗尽前能够接入电源")
-        confirm=input("输入Y以继续，输入N以重新确定区间：")
+        confirm=input("输入Y继续，输入N重新确定区间：")
         if confirm=="Y":
             begin=timer()    
-            points=fdprnbs(a,b)
+            points=prime_nums(a,b)
             plt.polar(points,points,'.',markersize=1)
             over=timer()
             TIME=over-begin
@@ -52,12 +55,12 @@ while data!="end":
             minutes=int(temp_minutes%60)
             print("用时：",hours,"时",minutes,"分",seconds,"秒")
             plt.show()
-            data=input("输入区间以继续，输入end退出程序：")
+            data=input("输入区间继续，输入end退出程序：")
         elif confirm=="N":
-            data=input("重新输入区间以继续，输入end退出程序：")
+            data=input("重新输入区间继续，输入end退出程序：")
     else:
         begin=timer()    
-        points=fdprnbs(a,b)
+        points=prime_nums(a,b)
         plt.polar(points,points,'.',markersize=1)
         over=timer()
         TIME=over-begin
@@ -68,4 +71,4 @@ while data!="end":
         minutes=int(temp_minutes%60)
         print("用时：",hours,"时",minutes,"分",seconds,"秒")
         plt.show()
-        data=input("输入区间以继续，输入end退出程序：")
+        data=input("输入区间继续，输入end退出程序：")
